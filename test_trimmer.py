74c1bc5a9b7d5a38cfc5fff078984d2d9411410f
yimpyomp <sebastian.enzi@gmail.com>
2024-03-29 04:03:22 -0500

    Add page of errors to beginning of output PDF Auto delete temporary files used in creating error page

--- test_trimmer.py
+++ test_trimmer.py
@@ -1,4 +1,7 @@
 from pypdf import PdfReader, PdfWriter
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import pathlib
 
 
 def load_test(test_path):
@@ -7,6 +10,7 @@ def load_test(test_path):
 
 
 def find_target_pages(test_reader, target_responses):
+    # Create
     target_pages = []
     last_page = 0
     upper_bound = test_reader._get_num_pages()
@@ -24,9 +28,16 @@ def find_target_pages(test_reader, target_responses):
     return target_pages
 
 
-def trim_test(target_pages, test_reader, save_name):
+def trim_test(target_pages, test_reader, error_path, save_name):
+    error_page = PdfReader(error_path)
+    error_path = pathlib.Path(error_path)
     save_path = save_name + '.pdf'
     trimmed_writer = PdfWriter()
+    # Add pages of error report
+    for page in error_page.pages:
+        trimmed_writer.add_page(page)
+    # Delete error report
+    pathlib.Path(error_path).unlink()
     for page in target_pages:
         trimmed_writer.add_page(test_reader.pages[page])
     trimmed_test = open(save_path, 'wb')
@@ -35,4 +46,33 @@ def trim_test(target_pages, test_reader, save_name):
     trimmed_test.close()
     print('Done')
     return None
+
+
+def make_error_page(error_file_path):
+    output_path = error_file_path.replace('.txt', '.pdf')
+    # Create a canvas
+    c = canvas.Canvas(output_path, pagesize=letter)
+
+    # Open the input text file
+    with open(error_file_path, 'r') as f:
+        lines = f.readlines()
+
+    # Set font and size
+    c.setFont("Helvetica", 12)
+
+    # Write each line to the PDF
+    y = 750  # Starting y position (you can adjust this)
+    for line in lines:
+        c.drawString(100, y, line.strip())
+        y -= 20  # Move to the next line
+
+    # Save the PDF
+    c.save()
+
+    # Delete text file
+    pathlib.Path(error_file_path).unlink()
+    return None
+
+
+
 
