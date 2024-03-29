630aa9381d8c81a7c34df778e7e7b9ea54db1b31
yimpyomp <sebastian.enzi@gmail.com>
2024-03-29 16:52:46 -0500

    Fix error causing program to fail on questions with a score value of null

--- json_parser.py
+++ json_parser.py
@@ -26,21 +26,24 @@ def filter_responses(responses: list) -> list:
         absolute_question_number = responses.index(entry) + 1
         next_entry = {}
         # Finding questions that were answered incorrectly
-        if entry["score"] < entry["max_score"]:
-            next_entry["Question"] = entry["item_reference"]
-            next_entry["Absolute question"] = "Question " + str(absolute_question_number)
-            # Convert response to proper letter if mcq, add whole answer otherwise
-            if entry["question_type"] == "mcq":
-                next_entry["Student Answer"] = convert_mcq(entry["response"]["value"][0])
-            else:
-                next_entry["Student Answer"] = entry["response"]["value"][0]
-            incorrect_responses.append(next_entry)
+        try:
+            if entry["score"] < entry["max_score"]:
+                next_entry["Question"] = entry["item_reference"]
+                next_entry["Absolute question"] = "Question " + str(absolute_question_number)
+                # Convert response to proper letter if mcq, add whole answer otherwise
+                if entry["question_type"] == "mcq":
+                    next_entry["Student Answer"] = convert_mcq(entry["response"]["value"][0])
+                else:
+                    next_entry["Student Answer"] = entry["response"]["value"][0]
+                incorrect_responses.append(next_entry)
+        except TypeError:
+            pass
     return incorrect_responses
 
 
 # Debug only
-def test_me():
-    sample_report = load_report('sample_data/responses.json')
+def test_me(sample_path):
+    sample_report = load_report(sample_path)
     sample_responses = filter_responses(sample_report)
     for entry in sample_responses:
         print(entry)
